f8c7706594ae60987a117d9cd923bb9d34f0c2fe
Hao Zhang <49357570+zhanghao5201@users.noreply.github.com>
2023-12-19 14:56:24 +0800

    Update _init_paths.py

--- tools/_init_paths.py
+++ tools/_init_paths.py
@@ -1,9 +1,3 @@
-# ------------------------------------------------------------------------------
-# pose.pytorch
-# Copyright (c) 2018-present Microsoft
-# Licensed under The Apache-2.0 License [see LICENSE for details]
-# Written by Bin Xiao (Bin.Xiao@microsoft.com)
-# ------------------------------------------------------------------------------
 
 from __future__ import absolute_import
 from __future__ import division
